4af3af645c7db2b6c5a44b2fdb51aa78cf3b2732
Datamaven1 <qayyumyussuf4@gmail.com>
2022-04-11 10:11:39 -0700

    All phabets exc e nd q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Descr: Print alphabets exc e nd q
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != e && c!= q)
+		{
+			putchar(c);
+		}
+		c++;
+
+	}
+
+	putchar('\n');
+	return (0);
+}
