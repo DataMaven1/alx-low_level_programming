02da2ceb2a38d8e08c636cd447527c820ee1e6cd
Datamaven1 <qayyumyussuf4@gmail.com>
2022-04-13 09:39:53 -0700

    Check loercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * is_lower- check for lowercase character
+ * _islower- check for lowercase character
  * @c: The character to be checked.
  *
  * Return: 1 if character is lowercase, 0 otherwise.
