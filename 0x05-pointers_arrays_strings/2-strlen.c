3afeb33c296277017e55513589a5e69e9922cf5d
Datamaven1 <qayyumyussuf4@gmail.com>
2022-04-21 18:29:48 -0700

    Strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - count the lenght of a string
+ * @s: string
+ * Return: the value of lenght i
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	return (i);
+}
