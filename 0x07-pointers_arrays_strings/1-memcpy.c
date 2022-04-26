5a4bc809e794eb7a94f3bf4a4202c014ace49333
datamaven1 <qayyumyussuf4@gmail.com>
2022-04-26 16:34:31 -0700

    TAsk 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,12 +10,9 @@
 char *_memcpy(char *dest, char *src, unsigned int n);
 {
 	unsigned int i;
-	i = 0
-		;
-	while (i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	return (dest);
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+
+	return(dest);
 }
