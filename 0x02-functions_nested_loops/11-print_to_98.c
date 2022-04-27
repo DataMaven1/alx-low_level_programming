500581eea04b705298cc525fe34067d127f9110f
datamaven1 <qayyumyussuf4@gmail.com>
2022-04-26 17:54:57 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,21 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98- print all antural numbers from input to 98
  * in order sepearyted by a comma followed by a space
  * @n: the number to begin counting at
+ * Return: void return nothing
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n < 98)
 	{
-		while (n > 98)
-			printf("%d, " n--);
-			printf("%d\n", n);
+		for (n = n; n < 98; n++)
+			printf("%d, ", n);
+		printf("%d\n", 98);
 	}
 	else
 	{
-		while (n < 98)
-			printf("%d, ", n++);
-			printf("%d\n", n);
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+		printf("%d\n", 98);
 	}
-i}
+}
