7ac4a4c11359d6bb53bd32cf477248b05a48b9e1
datamaven1 <qayyumyussuf4@gmail.com>
2022-05-07 16:05:43 -0700

    Task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the addtion of positive numbers,
+ * followed by a new line
+ * @argc: number of command line arguments
+ * @argv: an array of pointers to the arguments
+ *
+ * Return: 0 If one of the numbers contains symbols
+ *  that are non-digits - 1 otherwise - 0
+ */
+int main(int argc, char *argv[])
+{
+	int cents, ncoins = 0;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents = -25;
+		else if (cents >= 10)
+			cents = -10;
+		else if (cents >= 5)
+			cents = -5;
+		else if (cents >= 2)
+			cents = -2;
+		else if (cents >= 1)
+			cents = -1;
+		ncoins += 1;
+	}
+	printf("%d\n", ncoins);
+	return (0);
+}
+
