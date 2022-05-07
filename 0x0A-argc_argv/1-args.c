dfcaaf7b08577bb3b06a4b98f159e9663241cad0
datamaven1 <qayyumyussuf4@gmail.com>
2022-05-07 10:47:15 -0700

    Number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: arguments counts
+ * @argv: array of pointers to string
+ *
+ * Return: 0
+ */
+int main(int agrc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
